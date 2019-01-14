202f56ef131988a9ffe7536c8844a89c214dce5f
Sergey <xxsmotur@gmail.com>
2019-01-14 18:30:44 +0500

    Add more attributes to Build

--- structs/v1/build.go
+++ structs/v1/build.go
@@ -2,11 +2,17 @@ package v1
 
 // Build defines Born build
 type Build struct {
-	ID       int64     `json:"id"`
-	Name     string    `json:"name"`
-	Revision int64     `json:"revision"`
-	Steps    []Step    `json:"steps"`
-	Services []Service `json:"services,omitempty"`
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Revision    int64     `json:"revision"`
+	Status      string    `json:"status"`
+	Event       string    `json:"event"`
+	CreatedDate int64     `json:"created_date"`
+	StartedDate int64     `json:"started_date"`
+	Finished    int64     `json:"finished_date"`
+	Branch      string    `json:"branch"`
+	Steps       []Step    `json:"steps"`
+	Services    []Service `json:"services,omitempty"`
 }
 
 // Step defines for Build
