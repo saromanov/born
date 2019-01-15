4d15bc952c4d20709ef67fb4eebf01a2a123810e
Sergey <xxsmotur@gmail.com>
2019-01-15 18:43:54 +0500

    cmd: Add setupProvider method

--- cmd/born/main.go
+++ cmd/born/main.go
@@ -1,1 +1,12 @@
-package born
+package main
+
+import (
+	"github.com/saromanov/born/provider"
+	"github.com/saromanov/born/provider/github"
+)
+
+// setupProvider provides initialization of provider
+// at this moment, its support only Github
+func setupProvider(name string) (provider.Provider, error) {
+	github.New(github.Options{})
+}
