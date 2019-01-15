9fd58be4a745fed1003f5333d688c9ab82a1d201
Sergey <xxsmotur@gmail.com>
2019-01-15 18:45:51 +0500

    init of flags

--- cmd/born/main.go
+++ cmd/born/main.go
@@ -3,7 +3,21 @@ package main
 import (
 	"github.com/saromanov/born/provider"
 	"github.com/saromanov/born/provider/github"
+	"github.com/urfave/cli"
 )
+
+var flags = []cli.Flag{
+	cli.StringFlag{
+		EnvVar: "GITHUB_TOKEN",
+		Name:   "github-token",
+		Usage:  "using of github token for test",
+	},
+	cli.StringFlag{
+		EnvVar: "BORN_SERVER",
+		Name:   "born-server",
+		Usage:  "server of the born",
+	},
+}
 
 // setupProvider provides initialization of provider
 // at this moment, its support only Github
