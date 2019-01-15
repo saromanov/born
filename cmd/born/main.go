7239b2c65949ceb62d8c5953cf6afaf7c4a1f7b5
Sergey <xxsmotur@gmail.com>
2019-01-15 22:15:13 +0500

    setup of github provider

--- cmd/born/main.go
+++ cmd/born/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+
+	"github.com/google/go-github/github"
 	"github.com/saromanov/born/provider"
 	"github.com/saromanov/born/provider/github"
 	"github.com/urfave/cli"
+	"golang.org/x/oauth2"
 )
 
 var flags = []cli.Flag{
@@ -21,6 +25,15 @@ var flags = []cli.Flag{
 
 // setupProvider provides initialization of provider
 // at this moment, its support only Github
-func setupProvider(name string) (provider.Provider, error) {
-	github.New(github.Options{})
+func setupProvider(c *cli.Context) (provider.Provider, error) {
+	token := c.String("github-token")
+	ctx := context.Background()
+	sts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, sts)
+	client := github.NewClient(tc)
+	return github.New(github.Options{
+		Client: client
+	})
 }
