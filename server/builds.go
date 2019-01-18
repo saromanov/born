72f1c51937a1bac48463e8f24dc0f520101015f1
Sergey <xxsmotur@gmail.com>
2019-01-18 20:55:29 +0500

    server:filling of the method createBuild

--- server/builds.go
+++ server/builds.go
@@ -3,10 +3,22 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+
+	structs "github.com/saromanov/drone/structs/v1"
 )
 
 // createBuild provides creating of the new build
 func createBuild(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("X-BORN-TOKEN")
-	fmt.Println(token)
+	decoder := json.NewDecoder(r.Body)
+	var payload *structs.BuildRequest
+	err = decoder.Decode(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		jsonapi.WriteBasicError(w, "400", err.Error(), "Cannot create channel")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
