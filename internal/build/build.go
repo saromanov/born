3298af76f5f02577a3a60ea915d6f1aa263dddb7
Sergey <xxsmotur@gmail.com>
2019-01-18 21:22:07 +0500

    Add puppet method for create build

---
+++ internal/build/build.go
@@ -0,0 +1,8 @@
+// Package build provides creating, starting and
+// handling of builds
+package build
+
+// Create method provides creating of the build
+func Create(repo string) error {
+	return nil
+}
