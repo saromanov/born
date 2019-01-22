01e442d6ad7295c90b6354641acea12e5e5a8eae
Sergey <xxsmotur@gmail.com>
2019-01-23 00:59:12 +0500

    build: fixing of argument

--- internal/build/build.go
+++ internal/build/build.go
@@ -2,7 +2,16 @@
 // handling of builds
 package build
 
+import structs "github.com/saromanov/born/structs/v1"
+
 // Create method provides creating of the build
-func Create(repo string) error {
+func Create(u *structs.User, repo string) error {
+	return nil
+}
+
+// getBornFile provides getting of the .born.yml file
+// from the repo
+func getBornFile(repo string) error {
+
 	return nil
 }
