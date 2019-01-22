013eef648f7a5425c23693fe263cea24c3196e0f
Sergey <xxsmotur@gmail.com>
2019-01-23 01:05:27 +0500

    add method for generate random string

---
+++ internal/build/random.go
@@ -0,0 +1,17 @@
+package build
+
+import "math/rand"
+
+const (
+	letterBytes     = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	imageNameLength = 20
+)
+
+// randomString provides generation of the random string
+func randomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
