4467ee18062a607f69ec4e64832e27516f5ab96b
Sergey <xxsmotur@gmail.com>
2019-01-27 15:19:13 +0500

    register of endpoint for create build

--- server/server.go
+++ server/server.go
@@ -5,12 +5,25 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/qor/auth"
 	"github.com/qor/auth/providers/password"
 	"github.com/qor/session/manager"
 )
 
-const defaultAddress = "127.0.0.1:2310"
+const (
+	defaultAddress = "127.0.0.1:2310"
+	contentType    = "application/json"
+)
+
+type handler struct {
+	handler func(http.ResponseWriter, *http.Request)
+}
+
+func (handle *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", contentType)
+	handle.handler(w, r)
+}
 
 // Health provides healthcheck of the server
 func Health(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +35,13 @@ func Setup(addr string) {
 	if addr == "" {
 		addr = defaultAddress
 	}
-	mux := http.NewServeMux()
+	router := mux.NewRouter().StrictSlash(true)
+	router.Handle("/v1/builds", &handler{handler: createBuild}).Methods("POST")
 	authData := auth.New(&auth.Config{})
 	authData.RegisterProvider(password.New(&password.Config{}))
 
-	mux.Handle("/auth/", authData.NewServeMux())
-	http.HandleFunc("/health", Health)
+	router.Handle("v1//auth/", authData.NewServeMux())
+	router.HandleFunc("v1/health", Health)
 	log.Printf("starting server at %s", addr)
-	log.Fatal(http.ListenAndServe(addr, manager.SessionManager.Middleware(mux)))
+	log.Fatal(http.ListenAndServe(addr, manager.SessionManager.Middleware(router)))
 }
