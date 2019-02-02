822d759620e27186e0bd3fa483ce3b06cc4f7044
Sergey <xxsmotur@gmail.com>
2019-02-02 21:57:50 +0500

    add unzip method

--- internal/build/files.go
+++ internal/build/files.go
@@ -1,7 +1,12 @@
 package build
 
 import (
+	"archive/zip"
+	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/saromanov/godownload"
 )
@@ -13,5 +18,62 @@ func downloadRepo(link string) error {
 		Archive: "zip",
 	}
 	gd.Download(link, nil)
+	return nil
+}
+
+// unzip provides unzipping of the dir to output folder
+func unzip(src, dest string) error {
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		fpath := filepath.Join(dest, f.Name)
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+
+		} else {
+
+			// Make File
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return err
+			}
+
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+
+			_, err = io.Copy(outFile, rc)
+
+			// Close the file without defer to close before next iteration of loop
+			outFile.Close()
+
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+
 	return nil
 }
