a3048cc4983434782539bbc583e148c9281b46d6
Sergey <xxsmotur@gmail.com>
2019-02-11 16:59:42 +0500

    Add pre-script argument

--- structs/v1/config.go
+++ structs/v1/config.go
@@ -2,9 +2,10 @@ package v1
 
 // Config provides definition of the config at .born.yml
 type Config struct {
-	Pipeline string                 `yaml:"pipeline"`
-	Name     string                 `yaml:"name"`
-	Steps    map[string]interface{} `yaml:"steps"`
+	Pipeline  string                 `yaml:"pipeline"`
+	Name      string                 `yaml:"name"`
+	PreScript []string               `yaml:"pre_script"`
+	Steps     map[string]interface{} `yaml:"steps"`
 }
 
 // StepConfig defines step at the .born.yml
